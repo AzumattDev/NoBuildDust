48db758c71a886f11f5d17906da799f780fede53
AzumattDev <azumattgaming@gmail.com>
2022-11-26 03:05:39 -0500

    TS release.

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,6 @@ namespace NoBuildDust
         internal const string ModVersion = "1.0.0";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
-        private static string ConfigFileName = ModGUID + ".cfg";
 
         private readonly Harmony _harmony = new(ModGUID);
 
